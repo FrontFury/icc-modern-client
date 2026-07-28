add63ed2d42bc3cb828893b260eb4a6bfc0a46a7
estiakahamed01 <tasin220116@diit.edu.bd>
2026-07-28 20:26:21 +0600

    Added History Section

--- src/pages/About/About/About.jsx
+++ src/pages/About/About/About.jsx
@@ -1,9 +1,11 @@
 import AboutBanner from '../AboutBanner/AboutBanner';
+import HistorySection from '../HistorySection/HistorySection';
 
 const About = () => {
     return (
         <div>
             <AboutBanner></AboutBanner>
+            <HistorySection></HistorySection>
         </div>
     );
 };
